a5866e25753e581d8a06f407a5d2218308f1ae15
Adem YALÇIN <ademyalcin@Adems-MacBook-Pro.local>
2021-01-08 21:38:19 +0300

    done

--- src/assets/js/vendors/index.js
+++ src/assets/js/vendors/index.js
@@ -3,8 +3,35 @@ import 'slick-carousel';
 
 $(function() {
    $('#work').slick({
-    dots: true,
-    slidesPerRow: 4,
-    rows: 2,
+      dots: true,
+      slidesPerRow: 4,
+      rows: 2,
+      arrows:false,
+      responsive: [
+         {
+           breakpoint: 992,
+           settings: {
+            slidesPerRow: 3,
+            rows: 2
+           }
+         },
+         {
+           breakpoint: 600,
+           settings: {
+            slidesPerRow: 2,
+            rows: 1
+           }
+         },
+         {
+           breakpoint: 480,
+           settings: {
+            slidesPerRow: 1,
+            rows: 1
+           }
+         }
+      ],
+       customPaging : function(slider, i) {
+         return '<li><button></button></li>';
+     },
    });
 });
